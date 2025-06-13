107ee7ad114145366427c1517ff702bab4598573
Dyaa Mohamed <dyaamohamed86@gmail.com>
2025-06-13 17:55:01 +0300

    added identity configurations and made the login endpoint

--- IlmPath.Infrastructure/DependencyInjections.cs
+++ IlmPath.Infrastructure/DependencyInjections.cs
@@ -4,10 +4,13 @@ using IlmPath.Infrastructure.Categories.Persistance;
 using IlmPath.Infrastructure.Courses.Presistance;
 using IlmPath.Infrastructure.Data;
 using IlmPath.Infrastructure.Seed;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 namespace IlmPath.Infrastructure;
 
 public static class DependencyInjection
@@ -16,7 +19,32 @@ public static class DependencyInjection
     {
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-        services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+        services.AddIdentity<ApplicationUser, IdentityRole>() 
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            //options.SaveToken = true;
+            //options.RequireHttpsMetadata = false; // Set to true in production
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidAudience = configuration["JWT:ValidAudience"],
+                ValidIssuer = configuration["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+            };
+        });
+        
         services.AddScoped<ICategoriesRepository, CategoriesRepository>();
         services.AddScoped<DataSeeder>();
         services.AddScoped<IdentitySeeder>();
