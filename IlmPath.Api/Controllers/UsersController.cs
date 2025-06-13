f33e0289c00b02d99dbdd80d81901f844248548f
Dyaa Mohamed <dyaamohamed86@gmail.com>
2025-06-13 20:16:59 +0300

    added update profile image endpoint

--- IlmPath.Api/Controllers/UsersController.cs
+++ IlmPath.Api/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 
 using IlmPath.Application.Users.Commands;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using YourApp.Application.Features.Authentication.Commands;
@@ -30,5 +31,24 @@ public class UsersController : ControllerBase
     {
         var result = await _mediator.Send(command);
         return Ok(new { UserId = result });
+    }
+
+    [HttpPost("{userId}/profile-image")]
+    public async Task<IActionResult> UpdateProfileImage(string userId, IFormFile profileImage)
+    {
+        if (profileImage == null || profileImage.Length == 0)
+        {
+            return BadRequest("Image file is required.");
+        }
+
+        var command = new UpdateProfileImageCommand
+        {
+            UserId = userId,
+            ProfileImage = profileImage
+        };
+
+        var imageUrl = await _mediator.Send(command);
+
+        return Ok(new { ProfileImageUrl = imageUrl });
     }
 } 
--- IlmPath.Infrastructure/Data/Migrations/20250613163439_ProfileImageForUser.Designer.cs
+++ IlmPath.Infrastructure/Data/Migrations/20250613163439_ProfileImageForUser.Designer.cs
@@ -4,6 +4,7 @@ using IlmPath.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IlmPath.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250613163439_ProfileImageForUser")]
+    partial class ProfileImageForUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
