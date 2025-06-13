11b57c84704da5bf02918fcb5791317a2ee0fc10
Abdelrahman Alaa <abdelrahmanalaa107@gmail.com>
2025-06-13 21:07:35 +0300

    create enrollment

---
+++ IlmPath.Application/Mappings/EnrollmentMappings.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using IlmPath.Application.Categories.Commands.CreateCategory;
+using IlmPath.Application.Categories.Commands.UpdateCategory;
+using IlmPath.Application.Categories.DTOs.Requests;
+using IlmPath.Application.Categories.DTOs.Responses;
+using IlmPath.Application.Enrollments.Commands.CreateEnrollment;
+using IlmPath.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlmPath.Application.Mappings
+{
+    class EnrollmentMappings : Profile
+    {
+        public EnrollmentMappings()
+        {
+            // Domain to Response DTO
+            //CreateMap<Category, CategoryResponse>();
+            CreateMap<CreateEnrollmentCommand, Enrollment>();
+
+
+            // Request DTO to Command
+            //CreateMap<CreateCategoryRequest, CreateCategoryCommand>();
+
+            // For UpdateCategoryCommand, we need to handle the Id parameter
+            CreateMap<(UpdateCategoryRequest Request, int Id), UpdateCategoryCommand>()
+                .ConstructUsing(src => new UpdateCategoryCommand(src.Id, src.Request.Name, src.Request.Slug));
+        }
+    }
+}
---
+++ IlmPath.Infrastructure/Enrollments/presistance/EnrollmentRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IlmPath.Application.Common.Interfaces;
+using IlmPath.Domain.Entities;
+using IlmPath.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace IlmPath.Infrastructure.Enrollments.presistance
+{
+    class EnrollmentRepository : IEnrollmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnrollmentRepository(ApplicationDbContext context )
+        {
+            _context=context;
+        }
+        public async Task AddEnrollmentAsync(Enrollment enrollment)
+        {
+           await _context.Enrollments.AddAsync(enrollment);
+           await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEnrollmentAsync(int id)
+        {
+           var enrollment=  await _context.Enrollments.FindAsync(id);
+            if(enrollment == null)
+                throw new Exception("Category not found");
+
+            _context.Enrollments.Remove(enrollment);
+            await _context.SaveChangesAsync();
+
+        }
+
+        public async Task<List<Enrollment>> GetAllEnrollmentsAsync()
+        {
+            return await _context.Enrollments.ToListAsync();
+        }
+
+        public async Task<Enrollment?> GetEnrollmentByIdAsync(int id)
+        {
+          return  await _context.Enrollments.FindAsync(id);
+        }
+
+        public async Task UpdateEnrollmentAsync(Enrollment enrollment)
+        {
+            var existingEnrollment = await _context.Enrollments.FindAsync(enrollment.Id);
+            if (existingEnrollment == null)
+                throw new Exception("Category not found");
+
+            existingEnrollment.EnrollmentDate = enrollment.EnrollmentDate;
+            existingEnrollment.PricePaid= enrollment.PricePaid;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
